3faef951fdde2955b289d8356192f08df6b3394b
harshadatta099 <harshaa@smbxl.com>
2023-08-01 14:49:45 +0530

    Updated!

--- src/Auth.jsx
+++ src/Auth.jsx
@@ -1,42 +1,21 @@
-import React,{useEffect} from 'react';
-import { Navigate } from 'react-router-dom';
-
-const isAuthenticated = () => {
-  return JSON.parse(localStorage.getItem('isLoggedIn'));
-};
-
-export const ProtectedRoute = ({ element }) => {
-  if (!isAuthenticated()) {
-    return <Navigate to="/" />;
-  }
-
-  return element;
-};
-
-export const ProtectedSignupRoute = ({ element }) => {
-  if (isAuthenticated()) {
-    return <Navigate to="/user" />;
-  }
-
-  return element;
-};
+// AuthGuard.js
+import React, { useEffect } from 'react';
+import { Navigate, Route } from 'react-router-dom';
 
 export const AuthGuard = ({ element }) => {
   const isLoggedIn = JSON.parse(localStorage.getItem('isLoggedIn'));
+  const roleId = localStorage.getItem('roleId');
+  const userRole = roleId === '1' ? 'user' : roleId === '2' ? 'hr' : roleId === '3' ? 'admin' : '';
+
   useEffect(() => {
-    const roleId = localStorage.getItem('roleId');
-    const userRole = roleId === 1 ? 'user' : roleId === 2 ? 'hr' : roleId === 3 ? 'admin' : '';
     localStorage.setItem('userRole', userRole);
-  }, []);
+  }, [userRole]);
+
   return isLoggedIn ? <Navigate to={`/${userRole}`} /> : element;
 };
 
-
-const userRole = localStorage.getItem('roleId') === '1' ? 'user' : localStorage.getItem('roleId') === '2' ? 'hr' : localStorage.getItem('roleId') === '3' ? 'admin' : ''
 export const PrivateRoute = ({ role, children }) => {
-  if (userRole === role) {
-    return children
-  } else {
-    return <Navigate to='/' />
-  }
-}
+  const userRole = localStorage.getItem('userRole');
+
+  return userRole === role ? children : <Navigate to="/" />;
+};
