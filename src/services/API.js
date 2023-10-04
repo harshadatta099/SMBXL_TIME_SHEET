cacff5d2c62e8696c99f08a9d72c46a95fdba844
harshadatta099 <harshaa@smbxl.com>
2023-10-04 14:24:39 +0530

    Added getNetworkIP function

--- src/services/API.js
+++ src/services/API.js
@@ -1,6 +1,15 @@
 import axios from 'axios'
 
-export const BASE_URL = 'http://localhost:5070';
+
+
+const getNetworkIP = () => {
+  if (window.location.hostname === '192.168.1.2') {
+    return 'http://192.168.1.2:5070';
+  } else if (window.location.hostname === '192.168.200.2') {
+    return 'http://192.168.200.2:5070';
+  } 
+};
+export const BASE_URL = getNetworkIP();
 
 const getTokenIdFromLocalStorage = () => {
   return localStorage.getItem('tokenid');
